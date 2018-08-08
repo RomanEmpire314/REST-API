47b80153336b02f8c56bab85b9889119004e98c2
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2018-08-07 23:27:34 -0400

    vehicle class

---
+++ src/hello/Vehicle.java
@@ -0,0 +1,26 @@
+package hello;
+
+public class Vehicle extends CarAuctionRESTCall {
+	private String $class="org.acme.vehicle.auction.Vehicle";
+	private String vin;
+	private String owner;
+	public String get$class() {
+		return $class;
+	}
+	public void set$class(String $class) {
+		this.$class = $class;
+	}
+	public String getVin() {
+		return vin;
+	}
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+}
