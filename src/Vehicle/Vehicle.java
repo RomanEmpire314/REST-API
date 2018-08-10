29abdd551b46b6e4c743d9405f41e6b6981ef588
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2018-08-10 03:16:21 -0400

    new changes

---
+++ src/Vehicle/Vehicle.java
@@ -0,0 +1,64 @@
+package Vehicle;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import general.CarAuctionRESTCall;
+
+public class Vehicle extends CarAuctionRESTCall {
+	private String $class="org.acme.vehicle.auction.Vehicle";
+	private String vin;
+	private String owner;
+	public String get$class() {
+		return $class;
+	}
+	public void set$class(String $class) {
+		this.$class = $class;
+	}
+	public String getVin() {
+		return vin;
+	}
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	 public String genJson()
+	  {
+		  String vehicle=null;
+		  ObjectMapper map= new ObjectMapper();
+		  map.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		  map.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		  try {
+			vehicle=map.writeValueAsString(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		  return vehicle;
+	  }
+	 public void getCopy(Vehicle vehicle)
+	    {
+	        this.vin=vehicle.getVin();
+	        this.owner=vehicle.getOwner();
+	    }
+	 public void jsonMap(String vehicleJson) 
+	  {
+			 try {
+				  ObjectMapper map= new ObjectMapper();
+		          Vehicle vehicle= new Vehicle();
+		          vehicle= map.readValue(vehicleJson, Vehicle.class);
+		          this.getCopy(vehicle);
+			 }   
+			 catch(IOException e)
+			 {
+				 e.printStackTrace();
+			 }
+	  }
+}
---
+++ src/Vehicle/VehicleListing.java
@@ -0,0 +1,99 @@
+package Vehicle;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import general.CarAuctionRESTCall;
+
+public class VehicleListing extends CarAuctionRESTCall {
+	private String $class="org.acme.vehicle.auction.VehicleListing";
+	private String listingID;
+	private double reservePrice;
+	private String description;
+	private ListingState state= ListingState.FOR_SALE;
+	enum ListingState{
+		FOR_SALE,
+		RESERVE_NOT_MET,
+		SOLD
+	}
+
+	public String get$class() {
+		return $class;
+	}
+
+	public void set$class(String $class) {
+		this.$class = $class;
+	}
+
+	public String getListingID() {
+		return listingID;
+	}
+
+	public void setListingID(String listingID) {
+		this.listingID = listingID;
+	}
+
+	public double getReservePrice() {
+		return reservePrice;
+	}
+
+	public void setReservePrice(double reservePrice) {
+		this.reservePrice = reservePrice;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ListingState getState() {
+		return state;
+	}
+
+	public void setState(ListingState state) {
+		this.state = state;
+	}
+	
+	 public String genJson()
+	  {
+		  String listing=null;
+		  ObjectMapper map= new ObjectMapper();
+		  map.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		  map.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		  try {
+			listing=map.writeValueAsString(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		  return listing;
+	  }
+	 
+	 public void getCopy(VehicleListing listing)
+	    {
+	        this.listingID= listing.getListingID();
+	        this.description=listing.getDescription();
+	        this.reservePrice=listing.getReservePrice();
+	        this.state=listing.getState();
+	    }
+	 
+	 public void jsonMap(String vehicleJson) 
+	  {
+			 try {
+				  ObjectMapper map= new ObjectMapper();
+		          VehicleListing vehicle= new VehicleListing();
+		          vehicle= map.readValue(vehicleJson, VehicleListing.class);
+		          this.getCopy(vehicle);
+			 }   
+			 catch(IOException e)
+			 {
+				 e.printStackTrace();
+			 }
+	  }
+
+}
